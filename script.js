8355d598ef8209ce3ec00a0afcad91f13f5249d5
DeadlyOmega <vincentkaczor@fn.de>
2025-02-20 17:28:01 +0100

    a

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Update the accent color dynamically when the color picker changes
-    const accentInput = document.getElementById('accentColor');
-    accentInput.addEventListener('input', () => {
-      document.documentElement.style.setProperty('--accent-color', accentInput.value);
-    });
-  });
-  console.clear();
-Splitting();
